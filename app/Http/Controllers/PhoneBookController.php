81cafed1917af304d7d058ac3f8538b4f19136fb
Rabbi <mdrabbyrabby96@gmail.com>
2022-04-20 01:51:49 +0600

    first commit

---
+++ app/Http/Controllers/PhoneBookController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PhoneBook;
+use Illuminate\Http\Request;
+
+class PhoneBookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = PhoneBook::all();
+        return view('phone_book.index',compact('contacts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('phone_book.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        PhoneBook::create($request->all());
+        return redirect()->route('phone-book.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PhoneBook  $phoneBook
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $contact = PhoneBook::find($id);
+        return view('phone_book.show',compact('contact'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PhoneBook  $phoneBook
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $contact = PhoneBook::find($id);
+        return view('phone_book.edit', compact('contact'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PhoneBook  $phoneBook
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $contact = PhoneBook::find( $id );
+        $contact->update($request->all());
+        return redirect()->route('phone-book.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PhoneBook  $phoneBook
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $contact = PhoneBook::find($id);
+
+        $contact->delete();
+        return redirect()->route('phone-book.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\PhoneBookController;
+use App\Models\PhoneBook;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('phone-book',[PhoneBookController::class,'index'])->name('phone-book.index');
+Route::post( 'phone-book', [PhoneBookController::class, 'store'] )->name( 'phone-book.store' );
+
+Route::get('phone-book/create',[PhoneBookController::class,'create'])->name('phone-book.create');
+
+Route::get('phone-book/{id}/edit',[PhoneBookController::class,'edit'])->name('phone-book.edit');
+
+Route::get( 'phone-book/{id}', [PhoneBookController::class, 'show'] )->name( 'phone-book.show' );
+Route::put( 'phone-book/{id}',[PhoneBookController::class,'update'])->name('phone-book.update');
+Route::delete( 'phone-book/{id}',[PhoneBookController::class,'destroy'])->name('phone-book.destroy');
